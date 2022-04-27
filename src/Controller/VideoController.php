ab086b4c8239a090a8ead611b7e5a0010baba6b2
Joseph Pierre Gomis <gomisjoepierre@gmail.com>
2022-04-27 12:52:37 +0000

    Change videos url

--- src/Controller/VideoController.php
+++ src/Controller/VideoController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/videos")
+ * @Route("/les-videos")
  */
 class VideoController extends AbstractController
 {
